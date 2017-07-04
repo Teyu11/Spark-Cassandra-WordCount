f07a3170af6758b4454f369441735e01ac420938
Shreya Krishnan <sshreyakrishnan@gmail.com>
2017-07-04 14:58:06 +0530

    DC/OS
    leader master's IP : 10.0.1.37
    Cassandra connection host  : 10.0.249.29

---
+++ WordCount1.scala
@@ -0,0 +1,27 @@
+/**
+  * Created by sskrishn on 3/7/17.
+  */
+
+import com.datastax.spark.connector._
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount1 {
+  def main(args:Array[String]) : Unit = {
+
+    val conf = new SparkConf(true).
+      setAppName("WordCount1").setMaster("mesos://10.0.1.37:5050").set("spark.cassandra.connection.host", "10.0.249.29")
+
+    val sc = new SparkContext(conf)
+
+    val sentences = sc.cassandraTable[(String)]("sentencesks", "sentences").select("sentence")
+
+    val textRDD = sc.parallelize(List(sentences.fold("")((f: String, s: String) => f + s)))
+
+    val splits = textRDD.flatMap(line => line.split(" ")).map(word =>(word,1))
+    val counts = splits.reduceByKey((x,y)=>x+y)
+
+    counts.saveToCassandra("sentencesks", "wordcount", SomeColumns("word", "count"))
+
+  }
+
+}
